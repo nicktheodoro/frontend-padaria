5814d317bd66cdf62354cf3f34d8fb5970720b9c
Nicolas Theodoro <nicolastheodoro@gmail.com>
2021-06-21 09:22:52 -0300

    crie renderização condicional na lista de produtos

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -28,34 +28,43 @@ export default class Products extends React.Component {
 
     return (
       <div className="container-products">
-        <Link to={"/products/register"} className="btn-add"> + Adicionar</Link>
+        <Link to={"/products/register"} className="btn-add">
+          {" "}
+          + Adicionar
+        </Link>
         <div className="container-product">
-          {produtos.map((product) => (
-            <div className="products" key={product.id}>
-              <div className="product-info">
-                <p>
-                  <strong>Id:</strong> {product.id}
-                </p>
-                <p>
-                  <strong>Nome:</strong> {product.nome}
-                </p>
-                <p>
-                  <strong>Valor:</strong> {formatForReal(product.valor)}
-                </p>
-                <Link
-                  to={`products/${product.id}/details`}
-                  className="btn-details"
-                >
-                  Detalhes
-                </Link>
-              </div>
-              <div className="product-img">
-                <p>
-                  <img src={product.urlImagem} alt={product.nome} />
-                </p>
-              </div>
+          {produtos.length == 0 ? (
+            <div>
+              <h1>Não tem produtos</h1>
             </div>
-          ))}
+          ) : (
+            produtos.map((product) => (
+              <div className="products" key={product.id}>
+                <div className="product-info">
+                  <p>
+                    <strong>Id:</strong> {product.id}
+                  </p>
+                  <p>
+                    <strong>Nome:</strong> {product.nome}
+                  </p>
+                  <p>
+                    <strong>Valor:</strong> {formatForReal(product.valor)}
+                  </p>
+                  <Link
+                    to={`products/${product.id}/details`}
+                    className="btn-details"
+                  >
+                    Detalhes
+                  </Link>
+                </div>
+                <div className="product-img">
+                  <p>
+                    <img src={product.urlImagem} alt={product.nome} />
+                  </p>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
     );
